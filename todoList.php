173688fbfdf1fbdc15bfda283daa899f43fcec3d
Antonio Andre <antonioandre1008@gmail.com>
2025-04-05 18:42:49 -0500

    realiza conexão com banco de dados

---
+++ todoList.php
@@ -0,0 +1,14 @@
+<?php
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_NAME'];
+$port = $_ENV['DB_PORT'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Falha na conexão: " . $conn->connect_error);
+} else {
+  echo "Conexão bem-sucedida com o banco de dados!";
+}

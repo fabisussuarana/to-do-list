cab7de1f47cc2ed2a0aed44c96a2df819959f688
Antonio Andre <antonioandre1008@gmail.com>
2025-04-05 18:53:00 -0500

    inclui bootstrap

--- todoList.php
+++ todoList.php
@@ -9,6 +9,24 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
   die("Falha na conexão: " . $conn->connect_error);
-} else {
-  echo "Conexão bem-sucedida com o banco de dados!";
 }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+  <title>Todo List PHP</title>
+</head>
+
+<body>
+  <h1>Hello</h1>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
